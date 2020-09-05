fc4b1f074ed7fac1941073dcf5665f224b6f5fc7
Kezia Baidoo <keziabaidoo100@gmail.com>
2020-09-05 13:57:24 +0100

    update text on home page

--- src/components/HomepageComponent.js
+++ src/components/HomepageComponent.js
@@ -1,90 +1,195 @@
-import React from 'react'
-import Logo from '../images/logo.png'
-import Footer from './footer'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import Logo from "../images/logo.png";
+import Footer from "./footer";
+import { Link } from "react-router-dom";
 
+class HomepageComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Username: "",
+      emailaddress: "",
+      password: "",
+      confirmpassword: "",
+      hospitalId: "",
+    };
 
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
-export default function HomepageComponent() {
+  render() {
     return (
-        <div>
-            <div className="homepage">
-                <div className="homepage-left">
-                    <img src={Logo} className="App-logo" alt="logo" />
-                    <h1 style={{ fontFamily: "Verdana", fontWeight: "bold", letterSpacing: "4px" }}>COVID-19 CASE</h1>
-                    <h1 style={{ fontFamily: "Verdana", fontWeight: "bold", letterSpacing: "4px" }}>MANAGER</h1>
-                    <h6 style={{ letterSpacing: "6px" }}>ONLINE DIAGNOSTIC SYSTEM</h6>
-                </div>
-                <div className="homepage-divider"></div>
-                <div className="login-section">
-                    <div class="login-wrap">
-                        <div class="login-html">
-                            <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab">Patient</label>
-                            <input id="tab-2" type="radio" name="tab" class="sign-up" /><label for="tab-2" class="tab">Health Professional</label>
-                            <div class="login-form">
-                                <div class="sign-in-htm">
-                                    <div class="group">
-                                        <label for="user" class="label">Username</label>
-                                        <input id="user" type="text" class="input input_anim" />
-                                        {/* <span class="focus-input_anim"></span> */}
-
-                                    </div>
-                                    <div class="group">
-                                        <label for="pass" class="label">Password</label>
-                                        <input id="pass" type="password" class="input input_anim" data-type="password" />
-                                        {/* <span class="focus-input_anim"></span> */}
-
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
-                                        <label class="form-check-label" for="defaultCheck1">
-                                            Keep me signed in
-                                        </label>
-                                    </div>
-                                    <div class="group">
-                                        <Link to="/test"><input type="submit" class="button" value="Sign In" /></Link>
-                                    </div>
-                                    <div class="hr"></div>
-                                    <div class="foot-lnk">
-                                        <a href="#forgot">Forgot Password?</a>
-                                        <p> Don't have an account? <a href="#signup">Sign up</a> </p>
-                                    </div>
-                                </div>
-                                <div class="sign-up-htm">
-                                    <div class="group">
-                                        <label for="user" class="label">Hospital ID</label>
-                                        <input id="user" type="text" class="input input_anim" />
-                                    </div>
-                                    <div class="group">
-                                        <label for="pass" class="label">Email Address</label>
-                                        <input id="pass" type="text" class="input input_anim" />
-                                    </div>
-                                    <div class="group">
-                                        <label for="pass" class="label">Password</label>
-                                        <input id="pass" type="password" class="input input_anim" data-type="password" />
-                                    </div>
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" />
-                                        <label class="form-check-label" for="defaultCheck1">
-                                            Keep me signed in
-                                        </label>
-                                    </div>
-                                    <div class="group">
-                                        <Link to="/portallogin"><input type="submit" class="button" value="Sign In" /></Link>
-                                    </div>
-                                    <div class="hr"></div>
-                                    <div class="foot-lnk">
-                                        <a href="#forgot">Forgot Password?</a> Don't have account?<a href="#signup"> Sign up</a>
-                                        {/* <p> Don't have an account?  </p> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+      <div>
+        <div className="homepage">
+          <div className="homepage-left">
+            <img src={Logo} className="App-logo" alt="logo" />
+            <h1
+              style={{
+                fontFamily: "Verdana",
+                fontWeight: "bold",
+                letterSpacing: "4px",
+              }}
+            >
+              COVID-19 CASE
+            </h1>
+            <h1
+              style={{
+                fontFamily: "Verdana",
+                fontWeight: "bold",
+                letterSpacing: "4px",
+              }}
+            >
+              MANAGER
+            </h1>
+            <h6 style={{ letterSpacing: "6px" }}>ONLINE DIAGNOSTIC SYSTEM</h6>
+          </div>
+          <div className="homepage-divider"></div>
+          <div className="login-section">
+            <div className="login-wrap">
+              <div className="login-html">
+                <input
+                  id="tab-1"
+                  type="radio"
+                  name="tab"
+                  className="sign-in"
+                  checked
+                />
+                <label for="tab-1" className="tab">
+                  Patient
+                </label>
+                <input id="tab-2" type="radio" name="tab" className="sign-up" />
+                <label for="tab-2" className="tab">
+                  Health Professional
+                </label>
+                <div className="login-form">
+                  <div className="sign-in-htm">
+                    <div className="group">
+                      <label for="user" className="label">
+                        Username
+                      </label>
+                      <input id="user" type="text" className="input input_anim"
+                       value={this.state.username}
+                       name="username"
+                       onChange={this.handleChange}
+                      
+                      />
+                      {/* <span class="focus-input_anim"></span> */}
                     </div>
+                    <div className="group">
+                      <label htmlFor="pass" className="label">
+                        Password
+                      </label>
+                      <input
+                        id="pass"
+                        type="password"
+                        className="input input_anim"
+                        data-type="password"
+                        value={this.state.password}
+                        name="password"
+                        onChange={this.handleChange}
+                      />
+                      {/* <span class="focus-input_anim"></span> */}
+                    </div>
+                    <div class="form-check">
+                      <input
+                        class="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="defaultCheck1"
+                      />
+                      <label className="form-check-label" htmlFor="defaultCheck1">
+                        Keep me signed in
+                      </label>
+                    </div>
+                    <div class="group">
+                      <Link to="/test">
+                        <input type="submit" className="button" value="Sign In" />
+                      </Link>
+                    </div>
+                    <div class="hr"></div>
+                    <div class="foot-lnk">
+                      <a href="#forgot">Forgot Password?</a>
+                      <p>
+                        {" "}
+                        Don't have an account? <a href="#signup">
+                          Sign up
+                        </a>{" "}
+                      </p>
+                    </div>
+                  </div>
+                  <div class="sign-up-htm">
+                    <div className="group">
+                      <label for="user" className="label">
+                        Hospital ID
+                      </label>
+                      <input id="user" type="text" className="input input_anim"
+                         value={this.state.hospitalId}
+                         name="hospitalId"
+                         onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className="group">
+                      <label htmlFor="pass" className="label">
+                        Email Address
+                      </label>
+                      <input id="pass" type="text" className="input input_anim"
+                         value={this.state.emailaddress}
+                         name="emailaddress"
+                         onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className="group">
+                      <label htmlFor="pass" className="label">
+                        Password
+                      </label>
+                      <input
+                        id="pass"
+                        type="password"
+                        className="input input_anim"
+                        data-type="password"
+                      />
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="defaultCheck1"
+                      />
+                      <label className="form-check-label" htmlFor="defaultCheck1">
+                        Keep me signed in
+                      </label>
+                    </div>
+                    <div className="group">
+                      <Link to="/portallogin">
+                        <input type="submit" className="button" value="Sign In" />
+                      </Link>
+                    </div>
+                    <div className="hr"></div>
+                    <div className="foot-lnk">
+                      <a href="#forgot">Forgot Password?</a> Don't have account?
+                      <a href=""> Sign up</a>
+                      {/* <p> Don't have an account?  </p> */}
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
-            <Footer />
+          </div>
         </div>
-
-    )
+        <Footer />
+      </div>
+    );
+  }
 }
+
+export default HomepageComponent;
